29dcd9c7e49183afa711532bacc42194d8e215d5
Elina <elina.zekunde@gmail.com>
2021-03-20 14:28:03 +0200

    - add content detail view - add next lesson button

---
+++ LearningApp/Resources/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  LearningApp
+//
+//  Created by Elīna Zekunde on 20/03/2021.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var videoHostUrl = "https://codewithchris.github.io/learningJSON/"
+}
